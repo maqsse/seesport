a546b853cf56744f53a5c6229658f58ab13e8e0c
max <32159805+maqsse@users.noreply.github.com>
2023-05-01 15:27:33 +0200

    Update Information.js

--- src/components/Information.js
+++ src/components/Information.js
@@ -3,6 +3,15 @@ import PropTypes from 'prop-types'
 import '../styles/accueil.css'
 import '../styles/information.css'
 
+/**
+Represents a component to display information about a nutrient.
+@param {Object} props - The props object.
+@param {string} props.icone - The path to the icon image.
+@param {number} props.nbGramme - The amount of the nutrient in grams or kilocalories.
+@param {string} props.type - The type of the nutrient (e.g. "Calories", "Protein", "Fat").
+@returns {JSX.Element} A JSX element that displays the nutrient information.
+*/
+
 const Information = ({ icone, nbGramme, type }) => {
   return (
     <div className='information'>
