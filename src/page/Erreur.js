d685de82486afafe551b165001c8d26505d2a3c9
max <32159805+maqsse@users.noreply.github.com>
2023-05-01 15:20:18 +0200

    Create Erreur.js

---
+++ src/page/Erreur.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Upbar from '../components/Upbar'
+import Sidebar from '../components/Sidebar'
+import { Link } from "react-router-dom";
+import '../styles/upbar.css'
+import '../styles/sidebar.css'
+import '../styles/error.css'
+
+const Erreur = () => {
+  return (
+    <div>
+      <div>
+      <Upbar/>
+    <article className='error'>
+      <h1> 404 </h1>
+      <h2> Oups! La page que vous demandez n'existe pas </h2>
+      <Link to="/user/12">Retour à la page d'accueil</Link>
+      <Sidebar/>
+    </article>
+    </div>
+    </div>
+  )
+}
+
+export default Erreur
