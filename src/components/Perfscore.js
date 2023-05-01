4cec077e73abb4b85402d8600258cfca8f6ec6df
max <32159805+maqsse@users.noreply.github.com>
2023-05-01 15:32:29 +0200

    Update Perfscore.js

--- src/components/Perfscore.js
+++ src/components/Perfscore.js
@@ -5,7 +5,13 @@ import { useState, useEffect } from 'react'
 import { RadialBar, RadialBarChart, Legend, ResponsiveContainer } from 'recharts'
 import '../styles/perfscore.css'
 
-
+/**
+ * Represents a performance score component.
+ * 
+ * @function
+ * @returns {JSX.Element} The JSX code representing the component.
+ * @param  {array} scoreUser
+  */
 
 
 const Perfscore = () => {
@@ -14,19 +20,30 @@ const Perfscore = () => {
   useEffect(() => {
     fetchScoreUser()
   }, [])
+  
+  /**
+   * Fetches the user's score information.
+   * @async
+   * @function
+   */
 
   async function fetchScoreUser () {
     const info = await fetchInformationScore()
     setScoreUser(info)
   }
 
-
+/**
+ * Formats score data.
+ * @param {Object} dataOriginal - The original data object.
+ * @param {Object} dataOriginal.data - The score data.
+ * @param {string} [dataOriginal.data.todayScore] - The score for today.
+ * @param {string} [dataOriginal.data.score] - The overall score.
+ * @param {string} dataOriginal.data.userId - The user ID.
+ * @returns {Array} - The formatted score data.
+ */
 
   return (
-    
-
-
-    <div className='score_wrap'>
+        <div className='score_wrap'>
       <h3 className='perfscore_h3'> Score </h3>
       <ResponsiveContainer width='100%' height='100%'>
         <RadialBarChart startAngle={140} endAngle={500} cx='50%' cy='50%' innerRadius={70} barSize={10} outerRadius={120} data={scoreUser}>
