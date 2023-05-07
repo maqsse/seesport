5f5b8708d1e9189c822ad239ddb318b227864280
max <32159805+maqsse@users.noreply.github.com>
2023-05-07 16:31:33 +0200

    Update Perfscore.js

--- src/components/Perfscore.js
+++ src/components/Perfscore.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { fetchInformationScore } from '../api/callApi'
 import { CustomLegendScore } from './Custom'
-import { useState, useEffect } from 'react'
 import { RadialBar, RadialBarChart, Legend, ResponsiveContainer } from 'recharts'
+import PropTypes from 'prop-types';
 import '../styles/perfscore.css'
 
 /**
@@ -14,56 +13,8 @@ import '../styles/perfscore.css'
   */
 
 
-const Perfscore = () => {
-  const [scoreUser, setScoreUser] = useState([])
-
-  useEffect(() => {
-    fetchScoreUser()
-  })
-  
-  /**
-   * Fetches the user's score information.
-   * @async
-   * @function
-   */
-
-  async function fetchScoreUser () {
-    const info = await fetchInformationScore()
-    const dataFormated = formatScore(info)
-    setScoreUser(dataFormated)
-  }
-
-/**
- * Formats score data.
- * @param {Object} dataOriginal - The original data object.
- * @param {Object} dataOriginal.data - The score data.
- * @param {string} [dataOriginal.data.todayScore] - The score for today.
- * @param {string} [dataOriginal.data.score] - The overall score.
- * @param {string} dataOriginal.data.userId - The user ID.
- * @returns {Array} - The formatted score data.
- */
+const Perfscore = ({scoreUser}) => {
  
- function formatScore (dataOriginal) {
-  const { data } = dataOriginal
-  let score
-  if (data.todayScore === undefined) {
-    score = data.score
-  } else {
-    score = data.todayScore
-  }
-  const newData = []
-  newData.push({
-    userId: data.userId,
-    todayScore: score * 100
-  })
-  newData.push({
-    userId: data.userId,
-    todayScore: 100,
-    fill: '#ffffff00'
-  })
-  return newData
-}
-
   return (
         <div className='score_wrap'>
       <h3 className='perfscore_h3'> Score </h3>
@@ -76,5 +27,15 @@ const Perfscore = () => {
     </div>
   )
 }
+
+Perfscore.propTypes = {
+  scoreUser: PropTypes.arrayOf(
+    PropTypes.shape({
+      userId: PropTypes.string.isRequired,
+      todayScore: PropTypes.number.isRequired,
+      fill: PropTypes.string,
+    })
+  ).isRequired,
+};
 
 export default Perfscore
